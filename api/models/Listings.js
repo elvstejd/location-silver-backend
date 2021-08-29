431c18130dd057aa2cf35d8c035ff25e36f8b605
elvstejd <elvistejeda0910@gmail.com>
2021-08-29 12:25:00 -0400

    Initial commit

---
+++ api/models/Listings.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const locationSchema = mongoose.Schema({
+    long: Number,
+    lat: Number
+});
+
+const listingSchema = mongoose.Schema({
+    address: String,
+    sector: String,
+    price: Number,
+    location: locationSchema
+});
+
+module.exports = mongoose.model('Listing', listingSchema);
---
+++ api/routes/listings.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const Listing = require('../models/Listings');
+
+/* GET all listings */
+router.get('/', async (req, res) => {
+    const listings = await Listing.find();
+    res.send(listings);
+});
+
+/* GET single listing provided an Id */
+router.get('/:id', async (req, res) => {
+    try {
+        const listing = await Listing.findOne({ _id: req.params.id });
+        if (!listing) {
+            res.status(404);
+            res.send({ error: "No listing by that id" });
+            return;
+        }
+        res.send(listing);
+    } catch {
+
+    }
+});
+
+/* POST a listing */
+router.post('/', async (req, res) => {
+    const listing = new Listing({
+        address: req.body.address,
+        sector: req.body.sector,
+        price: req.body.price,
+        location: {
+            long: req.body.location.long,
+            lat: req.body.location.lat,
+        }
+    });
+
+    await listing.save();
+    res.send(listing);
+});
+
+
+module.exports = router;
